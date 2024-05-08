4f2f2716a94a9add0a04315b7460c8d221b602da
mrrobot <wcgiraldo0@gmail.com>
2024-05-07 20:53:42 -0500

    Lifecycle in angular

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration: number = 0;
+  @Input({required: true}) message: string = '';
+
+  constructor() {
+    // NO ASYNC
+    // Before render
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    // Before and during render  
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    // After render
+    // ASYNC
+    console.log('ngOnInit');
+    // Showing the state of duration and message
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    console.log('-'.repeat(10));
+  }
+
+  ngAfterViewInit() {
+    // After render
+    // Child had been rendered
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+  }
+}
