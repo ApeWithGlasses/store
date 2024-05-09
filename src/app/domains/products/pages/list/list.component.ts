f96d12110c42cd1ab1353178a4a59cf749ff380b
mrrobot <wcgiraldo0@gmail.com>
2024-05-08 22:47:46 -0500

    Handling the cart with a service and signals

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
   selector: 'app-list',
@@ -13,7 +14,7 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
 })
 export class ListComponent {
   products = signal<Product[]>([]);
-  cart = signal<Product[]>([]);
+  private cartService = inject(CartService);
 
   constructor() {
     const initProducts: Product[] = [
@@ -79,6 +80,6 @@ export class ListComponent {
   }
 
   addToCart(product: Product) {
-    this.cart.update(prevState => [...prevState, product]);
+    this.cartService.addToCart(product);
   }
 }
--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -9,23 +10,17 @@ import { Product } from '../../models/product.model';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Input({required: true}) cart: Product[] = [];
-  total = signal(0);
+  private cartService = inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.total;
+
+  constructor() {
+
+  }
 
   hideSideMenu = signal(true);
 
   toggleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const cart = changes['cart'];
-    if (cart) {
-      this.total.set(this.calcTotal());
-    }
-  }
-
-  calcTotal(): number {
-    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 }
