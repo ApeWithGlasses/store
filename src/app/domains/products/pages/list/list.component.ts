a7c3c8abc1949556ef1ce6d2ba990a69c286dc0c
mrrobot <wcgiraldo0@gmail.com>
2024-05-08 21:51:32 -0500

    Header component integration

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -34,6 +35,41 @@ export class ListComponent {
         title: 'Product 3',
         price: 100,
         image: 'https://picsum.photos/640/640?r=14',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=4',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=42',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=64',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=44',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=9',
         creationAt: new Date().toISOString()
       }
     ]
