8a4e672b8a29488e07a1165cacd5e98660aef139
mrrobot <wcgiraldo0@gmail.com>
2024-05-09 22:43:54 -0500

    Creating our pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [UpperCasePipe, CurrencyPipe, DatePipe],
+  imports: [UpperCasePipe, CurrencyPipe, DatePipe, ReversePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
