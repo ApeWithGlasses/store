2a36dfc3462ed715c78b2d8356ead2ca06685b23
mrrobot <wcgiraldo0@gmail.com>
2024-05-14 23:16:42 -0500

    Category service implemented

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
+import { Category } from '@shared/models/category.model';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -14,18 +16,37 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getCategories();
+  }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
   }
 
-  ngOnInit(): void {
+  private getProducts() {
     this.productService.getAllProducts()
     .subscribe({
       next: (products) => {
         this.products.set(products);
+      },
+      error: () => {
+
+      }
+    });
+  }
+
+  private getCategories() {
+    this.categoryService.getAllCategories()
+    .subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
       },
       error: () => {
 
