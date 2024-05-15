b2264c7a989aab0681664f081f923c58de9e4b51
mrrobot <wcgiraldo0@gmail.com>
2024-05-14 23:02:01 -0500

    Getting product detail data and creating product gallery

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  product = signal<Product | null>(null);
+  cover = signal('');
+  private productService = inject(ProductService);
 
+  // Getting the id from the path params
+  @Input() id?: string;
+
+  ngOnInit(): void {
+    if (this.id) {
+      this.productService.getProduct(this.id)
+      .subscribe({
+        next: (product) => {
+          this.product.set(product);
+          if (product.images.length > 0) {
+            this.cover.set(product.images[0])
+          }
+        }
+      });
+    }
+  }
+
+  changeCover(newImg: string) {
+    this.cover.set(newImg);
+  }
 }
