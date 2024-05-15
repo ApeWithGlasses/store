aac5fd3bfffec976ee2d975098b22ada31926839
mrrobot <wcgiraldo0@gmail.com>
2024-05-14 23:43:42 -0500

    Code splitting and preloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './domains/products/pages/list/list.component';
 import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
 
@@ -12,11 +10,11 @@ export const routes: Routes = [
         children: [
             {
                 path: '',
-                component: ListComponent
+                loadComponent: () => import('./domains/products/pages/list/list.component')
             },
             {
                 path: 'about',
-                component: AboutComponent
+                loadComponent: () => import('./domains/info/pages/about/about.component')
             },
             {
                 path: 'product/:id',
